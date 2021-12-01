b1e09d66f9b37e2bc51acd649079d6a44d1210ce
ReinaldoBustamante <65493412+Spun1@users.noreply.github.com>
2021-12-01 15:53:17 -0300

    max questions

--- BD/cfedent.sql
+++ BD/cfedent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-11-2021 a las 17:50:24
+-- Tiempo de generación: 01-12-2021 a las 19:52:29
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.3.31
 
